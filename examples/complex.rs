790f784be94dd16e55a6dd9532e176680a4da37b
Fedor Gogolev <knsd@knsd.net>
2016-01-13 11:09:21 +0300

    Update documentation

--- examples/complex.rs
+++ examples/complex.rs
@@ -7,13 +7,14 @@ use daemonize::{Daemonize};
 fn main() {
     syslog::init(syslog::Facility::LOG_USER, log::LogLevelFilter::Debug, Some("daemon-name")).unwrap();
 
-    let daemonize = Daemonize::new().pid_file("/tmp/test.pid")
-                                    .chown_pid_file(true)
-                                    .working_directory("/tmp")
-                                    .user("nobody")
-                                    .group("daemon")
-                                    .group(2)
-                                    .privileged_action(|| "Executed before drop privileges");
+    let daemonize = Daemonize::new()
+        .pid_file("/tmp/test.pid") // Every method except `new` and `start`
+        .chown_pid_file(true)      // is optional, see `Daemonize` documentation
+        .working_directory("/tmp") // for default behaviour.
+        .user("nobody")
+        .group("daemon") // Group name
+        .group(2)        // Or group id
+        .privileged_action(|| "Executed before drop privileges");
 
     info!("{:?}", daemonize.start());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -20,18 +20,20 @@
 //! use daemonize::{Daemonize};
 //!
 //! fn main() {
-//!     let daemonize = Daemonize::new().pid_file("/tmp/test.pid")
-//!                                     .chown_pid_file(true)
-//!                                     .working_directory("/tmp")
-//!                                     .user("nobody")
-//!                                     .group("daemon") // Group name
-//!                                     .group(2) // Or group id
-//!                                     .privileged_action(|| "Executed before drop privileges");
-//!      match daemonize.start() {
-//!          Ok(_) => info!("Success, daemonized"),
-//!          Err(e) => error!("{}", e),
-//!      }
-//!  }
+//!     let daemonize = Daemonize::new()
+//!         .pid_file("/tmp/test.pid") // Every method except `new` and `start`
+//!         .chown_pid_file(true)      // is optional, see `Daemonize` documentation
+//!         .working_directory("/tmp") // for default behaviour.
+//!         .user("nobody")
+//!         .group("daemon") // Group name
+//!         .group(2)        // Or group id
+//!         .privileged_action(|| "Executed before drop privileges");
+//!
+//!     match daemonize.start() {
+//!         Ok(_) => info!("Success, daemonized"),
+//!         Err(e) => error!("{}", e),
+//!     }
+//! }
 //! ```
 
 mod ffi;
